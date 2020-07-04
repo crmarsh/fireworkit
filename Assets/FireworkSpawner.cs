e2fb60253e38e694dbe75eb6162da6eca23b655a
Chris Marsh <c.marshian@gmail.com>
2020-07-04 11:26:03 -0700

    Quick initial version, follow mouse, spawn firework prefab.

---
+++ Assets/FireworkSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> fireworkPrefabs = new List<GameObject>();
+
+    public void SpawnFirework(Vector3 atPos)
+    {
+        if (fireworkPrefabs == null || fireworkPrefabs.Count == 0)
+        {
+            Debug.LogError($"No fireworks prefabs in {name}");
+            return;
+        }
+
+        var randIndex = Random.Range(0, fireworkPrefabs.Count);
+        Instantiate(fireworkPrefabs[randIndex], atPos, Quaternion.identity, transform);
+    }
+}
---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class FollowMouse : MonoBehaviour 
+{
+    /// <summary>
+    /// Function definition for a button click event.
+    /// </summary>
+    [Serializable]
+    public class ClickedEvent : UnityEvent<Vector3> { }
+
+    [SerializeField] private float _distance = 10.0f;
+    [SerializeField] private float _scrollScale = 0.1f;
+    [SerializeField] ClickedEvent _onClick = null;
+
+    void Update()
+    {
+        if (!Camera.main)
+        {
+            return;
+        }
+
+        _distance += Input.mouseScrollDelta.y * _scrollScale;
+        _distance = Mathf.Clamp(_distance, Camera.main.nearClipPlane, Camera.main.farClipPlane);
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+        var p = ray.GetPoint(_distance);
+        transform.position = p;
+
+        if (Input.GetMouseButtonDown(0) && _onClick != null)
+        {
+            _onClick.Invoke(p);
+        }
+    }
+}
