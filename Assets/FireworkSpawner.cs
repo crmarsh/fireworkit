9fc2e8b606ee894494850153e7a51d1eaf1d3c39
Chris Marsh <c.marshian@gmail.com>
2023-07-04 09:33:11 -0700

    Weighted random fireworks

--- Assets/FireworkSpawner.cs
+++ Assets/FireworkSpawner.cs
@@ -1,10 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireworkSpawner : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> fireworkPrefabs = new List<GameObject>();
+    [Serializable]
+    public class Entry
+    {
+        public GameObject prefab;
+        public float weight;
+    }
+    [SerializeField] private List<Entry> fireworkPrefabs = new();
+
+    private float totalWeight = 0f;
+
+    private void UpdateSpawnTable()
+    {
+        totalWeight = 0.0f;
+        foreach (var entry in fireworkPrefabs)
+        {
+            totalWeight += entry.weight;
+        }
+    }
+
+    private GameObject RandomPrefab()
+    {
+        var r = UnityEngine.Random.Range(0.0f, totalWeight);
+        var weightSoFar = 0.0f;
+        for (var i = 0; i < fireworkPrefabs.Count - 1; i++)
+        {
+            weightSoFar += fireworkPrefabs[i].weight;
+            if (weightSoFar >= r)
+            {
+                return fireworkPrefabs[i].prefab;
+            }
+        }
+        return fireworkPrefabs[^1].prefab;
+    }
+
+    private void Start()
+    {
+        UpdateSpawnTable();
+    }
 
     public void SpawnFirework(Vector3 atPos)
     {
@@ -14,7 +52,6 @@ public class FireworkSpawner : MonoBehaviour
             return;
         }
 
-        var randIndex = Random.Range(0, fireworkPrefabs.Count);
-        Instantiate(fireworkPrefabs[randIndex], atPos, Quaternion.identity, transform);
+        Instantiate(RandomPrefab(), atPos, Quaternion.identity, transform);
     }
 }
